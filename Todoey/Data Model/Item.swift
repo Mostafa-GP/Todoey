8d6e48f448cc0002ccb027e882ea64bfb4423848
Mostafa Ahmed <mostafa@mostafas-macbook-pro.local>
2018-09-22 18:57:29 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mostafa Ahmed on 9/22/18.
+//  Copyright © 2018 Mostafa Ahmed. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
